1975cbbe4071eb4ddb7c629b5b9a6bd3d90676dc
vincent_foucault (elpimous12) <elpimous12@gmail.com>
2022-05-31 22:38:01 +0200

    added new pcan to moteus lib

--- include/wolf_ylo2_interface/ylo2_robot_hw.hpp
+++ include/wolf_ylo2_interface/ylo2_robot_hw.hpp
@@ -2,52 +2,18 @@
 #define YLO2_ROBOT_HW_H
 
 #include <wolf_hardware_interface/wolf_robot_hw.h>
-#include "moteus_driver/moteus_pcan_controller.h"
+#include "moteus_driver/YloTwoPcanToMoteus.hpp"
 #include <realtime_tools/realtime_publisher.h>
 #include <sensor_msgs/Imu.h>
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 
-namespace ylo22ros
-{
+namespace ylo22ros{
 
-struct MotorAdapter
-{
+class ylo2RobotHw : public hardware_interface::RobotHW, public hardware_interface::WolfRobotHwInterface{
 
 public:
 
-  MotorAdapter()
-  {
-    idx_ = -1;
-    sign_ = 1;
-    reduction_ = 1.0;
-  }
-
-  MotorAdapter(int idx, int sign, double reduction)
-  {
-    idx_ = idx;
-    sign_ = sign;
-    reduction_ = reduction;
-  }
-
-  const int& getIdx() {return idx_;}
-  const int& getSign() {return sign_;}
-  const double& getReduction() {return reduction_;}
-
-  void setIdx(int idx) {idx_ = idx;}
-  void setSign(int sign) {sign_ = sign;}
-  void setReduction(double reduction) {reduction_ = reduction;};
-
-private:
-
-  int idx_;
-  int sign_;
-  double reduction_;
-};
-
-class ylo2RobotHw : public hardware_interface::RobotHW, public hardware_interface::WolfRobotHwInterface
-{
-public:
   ylo2RobotHw();
   virtual ~ylo2RobotHw();
 
@@ -55,26 +21,12 @@ public:
   void read();
   void write();
 
+  float tmp_pos_ = 0.0;
+  float tmp_vel_ = 0.0;
+  float tmp_tor_ = 0.0;
+
+
 private:
-
-  void read_rx_queue(int motor_id, float& pos, float& vel, float& tor);
-
-  void send_tau(int motor_id, float tor);
-
-  void stop(int motor_id);
-
-  void send_query_only_command(int motor_id);
-
-  /** @brief moteus controller, enable communication with the motors (default is false) */
-  MoteusPcanControllerPtr motors_interface_;
-
-  std::vector<MotorAdapter> motor_adapters_;
-
-  /** @brief map to access specific Peak port, regarding to queried Id */
-  MoteusInterfaceMotorsMap motors_interface_map_ =
-  {
-    {"/dev/pcanpcifd0", {1,2,3,}}, {"/dev/pcanpcifd1", {4,5,6,}}, {"/dev/pcanpcifd2", {7,8,9,}}, {"/dev/pcanpcifd3", {10,11,12,}}
-  };
 
   /** @brief IMU realtime publisher */
   std::shared_ptr<realtime_tools::RealtimePublisher<sensor_msgs::Imu>> imu_pub_;
@@ -86,12 +38,8 @@ private:
   void startup_routine();
   std::mutex startup_routine_mutex;
 
-  float tmp_pos_;
-  float tmp_vel_;
-  float tmp_tor_;
-
 };
 
-}
+} // end namespace
 
 #endif
